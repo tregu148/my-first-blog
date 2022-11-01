fe8ef048792dae1f8ac754f441b966b04c11ca76
kakot <regulus.le0dra148@gmail.com>
2022-11-01 16:06:05 +0900

    added amont to plus/minus a number

--- blog/models.py
+++ blog/models.py
@@ -9,10 +9,33 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    amount = models.IntegerField()
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-
+    def plus1(self):
+        self.amount += 1
+        self.save() 
+        # saveしないと保存されない
+    def minus1(self):
+        self.amount -= 1
+        self.save()
     def __str__(self):
         return self.title
+    # def __init__(self):
+    #     self.amount = 0
+
+class Wallet(models.Model):
+    amount = models.IntegerField(0)
+
+    def plus1(self):
+        self.amount += 1
+    def minus1(self):
+        self.amount -= 1
+
+    def __str__(self):
+        return f"{self.amount}"
+    # def __init__(self):
+    #     self.amount = 0
+        # self.save()
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
+from ctypes.wintypes import HENHMETAFILE
+from django.http import HttpResponseGone
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post
+from .models import Post, Wallet
 from .forms import PostForm
 from django.utils import timezone
 
@@ -7,10 +9,25 @@ from django.utils import timezone
 def post_list(request):
     # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = Post.objects.order_by('published_date')
+    # wallet = Wallet()
+    # wallet = get_object_or_404(Wallet,pk=1)
+    
     return render(request, 'blog/post_list.html',{'posts':posts})
+
+def init_wallet(request):
+    wallet = Wallet()
+    wallet.save()
+    return render(request,'blog/post_list.html',{'wallet':wallet})
+    # redirect('post_list')
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        if "plus_1_btn" in request.POST:
+            post.plus1()
+        elif "minus_1_btn" in request.POST:
+            post.minus1()
+        # return redirect('')
     return render(request, 'blog/post_detail.html',{'post':post})
 
 def post_new(request):
